400b3e2d33b0354a9f01530bfcd8a7f69e3b0ef6
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2023-07-06 23:33:31 +0700

    first commit init project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
-    name: 'nextjs',
-    script: 'yarn',
+    name: 'next-app',
+    script: 'npm',
     args: "start",
     cwd: "/var/www/next-app/",
     instances: 2,
