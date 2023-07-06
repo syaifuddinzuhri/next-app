d5d7ae07a7d118daf4968c9b2fec670d8dbf929c
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2023-07-06 23:32:30 +0700

    first commit init project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,18 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  apps: [{
+    name: 'nextjs',
+    script: 'yarn',
+    args: "start",
+    cwd: "/var/www/next-app/",
+    instances: 2,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production',
     }
-  }
+  }]
 };
